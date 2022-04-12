6bac972d521a1b9fe53718f057bf434bd72cb0c2
MehediHasan95 <imehedihasan95@gmail.com>
2022-04-12 20:41:43 +0600

    post

---
+++ js/posts.js
@@ -0,0 +1,20 @@
+function loadData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayData(data));
+}
+loadData();
+
+function displayData(posts) {
+  const postContainer = document.getElementById("post-container");
+  for (const elements of posts) {
+    const div = document.createElement("div");
+    div.classList.add("posts");
+    div.innerHTML = `
+    <h1>${elements.id}</h1>
+    <h3>Title: ${elements.title}</h3>
+    <p>${elements.body}</p>
+    `;
+    postContainer.appendChild(div);
+  }
+}
